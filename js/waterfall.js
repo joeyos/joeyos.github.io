ed66e2671893b0fed251b3102a88b3f7f051ef97
zhangquan1995 <zhangquanhao2010@sina.com>
2018-04-01 21:09:13 +0800

    Commit

--- js/waterfall.js
+++ js/waterfall.js
@@ -21,13 +21,6 @@
       core_tech: 'Qt',
       description: '利用Qt制作的腾讯电脑管家'
     }, {
-      demo_link: 'https://zhangquan1995.github.io/ImgChange',
-      img_link: 'http://i4.bvimg.com/639183/d4de18a5a9f1027f.png',
-      code_link: 'https://github.com/zhangquan1995/ImgChange',
-      title: '网页轮播图',
-      core_tech: 'JavaScript',
-      description: '图片自动切换，按钮切换'
-    }, {
       demo_link: 'http://v.youku.com/v_show/id_XMjgyNDEwMjU0OA==.html?spm=a2h0k.8191407.0.0&from=s1.8-1-1.2',
       img_link: 'http://i2.bvimg.com/639183/ce1e6453a5e06034.png',
       code_link: 'http://v.youku.com/v_show/id_XMjgyNDEwMjU0OA==.html?spm=a2h0k.8191407.0.0&from=s1.8-1-1.2',
@@ -35,19 +28,12 @@
       core_tech: 'Ae & Edius',
       description: '献给悄悄流逝的四年时光'
     }, {
-      demo_link: 'https://zhangquan1995.github.io/3DPhoto',
-      img_link: 'http://i4.bvimg.com/639183/a8ad6f92aa479097.png',
-      code_link: 'https://github.com/zhangquan1995/3DPhoto',
-      title: '3D图像切换',
-      core_tech: 'CSS3',
-      description: '百度前端学院3D图像切换'
-    }, {
-      demo_link: 'https://zhangquan1995.github.io/AdaptiveScreen',
-      img_link: 'http://i2.bvimg.com/639183/ea248b3f7c21815f.png',
-      code_link: 'https://github.com/zhangquan1995/AdaptiveScreen',
-      title: 'Adaptive Screen',
-      core_tech: 'CSS3',
-      description: 'div长宽比自适应屏幕尺寸'
+      demo_link: 'https://zhangquan1995.github.io/ImgChange',
+      img_link: 'http://i4.bvimg.com/639183/d4de18a5a9f1027f.png',
+      code_link: 'https://github.com/zhangquan1995/ImgChange',
+      title: '网页轮播图',
+      core_tech: 'JavaScript',
+      description: '图片自动切换，按钮切换'
     }, {
       demo_link: 'https://github.com/zhangquan1995/LicensePlateRecognition',
       img_link: 'http://i2.bvimg.com/639183/4fdd2d01185e473b.png',
@@ -62,6 +48,20 @@
       title: 'QQ局域网聊天',
       core_tech: 'Qt',
       description: 'A program made with Qt'
+    }, {
+      demo_link: 'https://zhangquan1995.github.io/AdaptiveScreen',
+      img_link: 'http://i2.bvimg.com/639183/ea248b3f7c21815f.png',
+      code_link: 'https://github.com/zhangquan1995/AdaptiveScreen',
+      title: 'Adaptive Screen',
+      core_tech: 'CSS3',
+      description: 'div长宽比自适应屏幕尺寸'
+    }, {
+      demo_link: 'https://zhangquan1995.github.io/3DPhoto',
+      img_link: 'http://i4.bvimg.com/639183/a8ad6f92aa479097.png',
+      code_link: 'https://github.com/zhangquan1995/3DPhoto',
+      title: '3D图像切换',
+      core_tech: 'CSS3',
+      description: '百度前端学院3D图像切换'
     }
   ];
 
